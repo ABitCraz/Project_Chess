6bb1d9676e81c4042aab2008fa67afd17380e36e
ABitCrazy <jiongxuyunfeng@foxmail.com>
2024-03-29 23:47:37 +0800

    Project Essentials

---
+++ Assets/Scripts/Models/Enums/ChessType.cs
@@ -0,0 +1,6 @@
+public enum ChessType
+{
+    King,
+    Queen,
+    Servant
+}
---
+++ Assets/Scripts/Objects/ChessPrototype/Chess.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class Chess{
+    public string ChessName;
+    public Sprite ChessSprite;
+    public Vector2 Position;
+    public ChessType ChessType;
+}

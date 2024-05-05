ee7ba36aed6aadfca18fe745c45e0f48083911a1
loffeat <dsliaoziyu@hotmail.com>
2024-05-06 01:56:41 +0800

    地形

--- Assets/Scripts/Models/Datum/ResourcePaths.cs
+++ Assets/Scripts/Models/Datum/ResourcePaths.cs
@@ -17,14 +17,14 @@ public class ResourcePaths
         new()
         {
             //LandscapeTypes
-            { LandscapeType.Ancient, "Images/Sprites/Landscape/Landscape" },
-            { LandscapeType.Canyon, "Images/Sprites/Landscape/Landscape" },
+            { LandscapeType.Ancient, "Images/Sprites/Landscape/L_Anicent" },
+            { LandscapeType.Canyon, "Images/Sprites/Landscape/L_Canyon" },
             { LandscapeType.Desert, "Images/Sprites/Landscape/L_Desert" },
             { LandscapeType.Highground, "Images/Sprites/Landscape/L_Highground" },
-            { LandscapeType.Ruin, "Images/Sprites/Landscape/Landscape" },
-            { LandscapeType.Wildlessness, "Images/Sprites/Landscape/Landscape" },
+            { LandscapeType.Ruin, "Images/Sprites/Landscape/L_Ruin" },
+            { LandscapeType.Wildlessness, "Images/Sprites/Landscape/L_Wildlessness" },
             //ConstructionTypes
-            { ConstructionType.City, "Images/Sprites/Construction/Construction" },
+            { ConstructionType.City, "Images/Sprites/Construction/L_City" },
             //ChessTypes
             { ChessType.Infantry, "Images/Sprites/Chess/Soldier/S_Soldier" },
             { ChessType.AA_Infantry, "Images/Sprites/Chess/AntiArmorSoldier/S_AntiArmorSoldier" },
@@ -38,21 +38,21 @@ public class ResourcePaths
         new()
         {
             //LandscapeTypes
-            { LandscapeType.Ancient, "Images/Sprites/Landscape/Landscape" },
-            { LandscapeType.Canyon, "Images/Sprites/Landscape/Landscape" },
+            { LandscapeType.Ancient, "Images/Sprites/Landscape/L_Anicent" },
+            { LandscapeType.Canyon, "Images/Sprites/Landscape/L_Canyon" },
             { LandscapeType.Desert, "Images/Sprites/Landscape/L_Desert" },
             { LandscapeType.Highground, "Images/Sprites/Landscape/L_Highground" },
-            { LandscapeType.Ruin, "Images/Sprites/Landscape/Landscape" },
-            { LandscapeType.Wildlessness, "Images/Sprites/Landscape/Landscape" },
+            { LandscapeType.Ruin, "Images/Sprites/Landscape/L_Ruin" },
+            { LandscapeType.Wildlessness, "Images/Sprites/Landscape/L_Wildlessness" },
             //ConstructionTypes
             { ConstructionType.City, "Images/Sprites/Construction/Construction" },
             //ChessTypes
-            { ChessType.Infantry, "Images/Sprites/Chess/Soldier/S_Soldier" },
-            { ChessType.AA_Infantry, "Images/Sprites/Chess/AntiArmorSoldier/S_AntiArmorSoldier" },
-            { ChessType.Light, "Images/Sprites/Chess/LightTank/S_LightTank" },
-            { ChessType.Heavy, "Images/Sprites/Chess/HeavyTank/S_HeavyTank" },
-            { ChessType.Artillery, "Images/Sprites/Chess/Artillery/Artillery" },
-            { ChessType.Mortar, "Images/Sprites/Chess/Mortar/S_Mortar" },
-            { ChessType.Commander, "Images/Sprites/Chess/General/S_General" },
+            { ChessType.Infantry, "Images/Sprites/Chess/Soldier/AC_Soldier" },
+            { ChessType.AA_Infantry, "Images/Sprites/Chess/AntiArmorSoldier/AC_AntiArmorSoldier" },
+            { ChessType.Light, "Images/Sprites/Chess/LightTank/AC_LightTank" },
+            { ChessType.Heavy, "Images/Sprites/Chess/HeavyTank/AC_HeavyTank" },
+            { ChessType.Artillery, "Images/Sprites/Chess/Artillery/AC_Artillery" },
+            { ChessType.Mortar, "Images/Sprites/Chess/Mortar/AC_Mortar" },
+            { ChessType.Commander, "Images/Sprites/Chess/General/AC_General" },
         };
 }
